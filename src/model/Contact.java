5513de608c6176f6fff795c9aecc434a111c63e5
azidinkun <azidinkun@icloud.com>
2024-11-11 18:58:22 +0600

    create a new class Contact

---
+++ src/model/Contact.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Contact {
+
+    private String name;
+    private String surname;
+    private String address;
+    private String phone;
+
+    public Contact(String name, String surname, String address, String phone) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
