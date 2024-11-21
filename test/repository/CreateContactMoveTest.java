83d20d0adfd604ab320f3db92cf4c073daa09d89
azidinkun <azidinkun@icloud.com>
2024-11-21 21:30:24 +0600

    covered the test for creating contact

--- test/repository/CreateContactMoveTest.java
+++ test/repository/CreateContactMoveTest.java
@@ -5,29 +5,49 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CreateContactMoveTest {
 
     CreateContactMove createContactMove;
+    List<Contact> contact;
 
-    List<Contact> contactList;
 
     @BeforeEach
     void setUp() {
+        contact = new ArrayList<>();
 
         createContactMove = new CreateContactMove();
-        Contact contactOne = new Contact("Azidin", "Amankulov", "Japan", "996777777777");
-        Contact contactTwo = new Contact("Aibek", "Mahronovich", "China", "9966666666");
-        contactList = List.of(contactOne, contactTwo);
     }
 
     @Test
-    @DisplayName("Tests to add contact to exist contacts")
-    public void CreateContactTest() {
+    @DisplayName("Tests to create contact")
+    public void createContactTest() {
 
+        String name = "Azidin\n";
+        String surname = "Amankulov\n";
+        String address = "Japan\n";
+        String number = "996777777777\n";
+
+
+        String input = name + surname + address + number;
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        createContactMove.setScanner(new Scanner(System.in));
+        createContactMove.createContact(contact);
+
+
+        assertEquals(1, contact.size(), "List size should be 1 after creating a contact");
+        assertEquals("Azidin", contact.getFirst().getName(), "Contact name should be 'Azidin'");
+        assertEquals("Amankulov", contact.getFirst().getSurname(), "Contact last name should be 'Amankulov'");
+        assertEquals("Japan", contact.getFirst().getAddress(), "Contact address should be 'Japan'");
+        assertEquals("996777777777", contact.getFirst().getPhone(), "Contact phone should be '996777777777'");
     }
+
+
 }
