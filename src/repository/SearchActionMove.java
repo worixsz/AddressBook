0b9bed3bc01198941b50e9dbbbb4deaa2af39df8
azidinkun <azidinkun@icloud.com>
2024-11-22 18:43:19 +0600

    deleted the methods to find by prefix

--- src/repository/SearchActionMove.java
+++ src/repository/SearchActionMove.java
@@ -5,7 +5,6 @@ import service.SearchAction;
 
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class SearchActionMove implements SearchAction {
 
@@ -17,117 +16,99 @@ public class SearchActionMove implements SearchAction {
 
     @Override
     public List<Contact> searchContactByName(List<Contact> contacts) {
-        System.out.print("Enter name to search: ");
-        String next = SC.next();
-        List<Contact> foundContacts = contacts.stream()
-                .filter(contact -> contact.getName().equals(next))
-                .collect(Collectors.toList());
-        if (!foundContacts.isEmpty()) {
+        try {
+            System.out.print("Enter name to search: ");
+            String next = SC.next();
+            List<Contact> foundContacts = contacts.stream()
+                    .filter(contact -> contact.getName().equals(next))
+                    .toList();
+            if (!foundContacts.isEmpty()) {
+                foundContacts.forEach(contact -> System.out.println("🔍 Contact Found: " + contact));
+            } else {
+                System.out.println("No contact found with the such name: " + next);
+                System.out.println("Trying to find similar contacts by name...");
+                findByNamePrefix(contacts, next);
+            }
 
-            foundContacts.forEach(contact -> System.out.println("🔍 Contact Found: " + contact));
-        } else {
-            System.out.println("No contact found with the such name: " + next);
-            System.out.println("Trying to find similar contacts by name...");
-            findByNamePrefix(contacts, next);
+        } catch (Exception e) {
+            e.addSuppressed(e.fillInStackTrace());
         }
-        return foundContacts;
+
+        return contacts;
     }
 
 
     @Override
     public List<Contact> searchContactBySurname(List<Contact> contacts) {
-        System.out.print("Enter surname to search: ");
-        String next = SC.next();
-        List<Contact> foundContacts = contacts.stream()
-                .filter(contact -> contact.getSurname().equals(next))
-                .collect(Collectors.toList());
-        if (!foundContacts.isEmpty()) {
+        try {
+            System.out.print("Enter surname to search: ");
+            String next = SC.next();
+            List<Contact> foundContacts = contacts.stream()
+                    .filter(contact -> contact.getSurname().equals(next))
+                    .toList();
+            if (!foundContacts.isEmpty()) {
 
-            foundContacts.forEach(contact -> System.out.println("🔍 Contact Found: " + contact));
-        } else {
-            System.out.println("No contact found with the such surname: " + next);
-            System.out.println("Trying to find similar contacts by surname...");
-            findBySurnamePrefix(contacts, next);
+                foundContacts.forEach(contact -> System.out.println("🔍 Contact Found: " + contact));
+            } else {
+                System.out.println("No contact found with the such surname: " + next);
+                System.out.println("Trying to find similar contacts by surname...");
+                findBySurnamePrefix(contacts, next);
+            }
+        } catch (Exception e) {
+            e.addSuppressed(e.fillInStackTrace());
         }
-        return foundContacts;
+
+        return contacts;
     }
 
     @Override
     public List<Contact> searchContactByAddress(List<Contact> contacts) {
-        System.out.print("Enter address to search: ");
-        String next = SC.next();
-        List<Contact> foundContacts = contacts.stream()
-                .filter(contact -> contact.getAddress().equals(next))
-                .collect(Collectors.toList());
-        if (!foundContacts.isEmpty()) {
+        try {
+            System.out.print("Enter address to search: ");
+            String next = SC.next();
+            List<Contact> foundContacts = contacts.stream()
+                    .filter(contact -> contact.getAddress().equals(next))
+                    .toList();
+            if (!foundContacts.isEmpty()) {
 
-            foundContacts.forEach(contact -> System.out.println("🔍 Contact Found: " + contact));
-        } else {
-            System.out.println("No contact found with the such address: " + next);
-            System.out.println("Trying to find similar contacts by address...");
-            findByAddressPrefix(contacts, next);
+                foundContacts.forEach(contact -> System.out.println("🔍 Contact Found: " + contact));
+            } else {
+                System.out.println("No contact found with the such address: " + next);
+                System.out.println("Trying to find similar contacts by address...");
+                findByAddressPrefix(contacts, next);
+            }
+
+        } catch (Exception e) {
+            e.addSuppressed(e.fillInStackTrace());
         }
-        return foundContacts;
+
+        return contacts;
     }
 
     @Override
     public List<Contact> searchContactByPhone(List<Contact> contacts) {
-        System.out.print("Enter phone number to search: ");
-        String next = SC.next();
-        List<Contact> foundContacts = contacts.stream()
-                .filter(contact -> contact.getPhone().equals(next))
-                .collect(Collectors.toList());
-        if (!foundContacts.isEmpty()) {
+        try {
+            System.out.print("Enter phone number to search: ");
+            String next = SC.next();
+            List<Contact> foundContacts = contacts.stream()
+                    .filter(contact -> contact.getPhone().equals(next))
+                    .toList();
+            if (!foundContacts.isEmpty()) {
 
-            foundContacts.forEach(contact -> System.out.println("🔍 Contact Found: " + contact));
-        } else {
-            System.out.println("No contact found with the such phone number: " + next);
-            System.out.println("Trying to find similar contacts by phone number...");
-            findByPhonePrefix(contacts, next);
-        }
-        return foundContacts;
-    }
+                foundContacts.forEach(contact -> System.out.println("🔍 Contact Found: " + contact));
+            } else {
+                System.out.println("No contact found with the such phone number: " + next);
+                System.out.println("Trying to find similar contacts by phone number...");
+                findByPhonePrefix(contacts, next);
+            }
 
-    @Override
-    public void findByNamePrefix(List<Contact> contacts, String namePrefix) {
-        List<Contact> filteredList = contacts.stream().filter(c -> c.getName().startsWith(namePrefix)).toList();
-        if (filteredList.isEmpty()) {
-            System.out.println("❗ No contacts found with the name prefix: " + namePrefix);
-        } else {
-            filteredList.forEach(smContact -> System.out.println("🔍 Similar contact: " + smContact));
+        } catch (Exception e) {
+            e.addSuppressed(e.fillInStackTrace());
         }
 
+        return contacts;
     }
 
-    @Override
-    public void findBySurnamePrefix(List<Contact> contacts, String surnamePrefix) {
-        List<Contact> filteredList = contacts.stream().filter(c -> c.getSurname().startsWith(surnamePrefix)).toList();
-        if (filteredList.isEmpty()) {
-            System.out.println("❗ No contacts found with the surname prefix: " + surnamePrefix);
-        } else {
-            filteredList.forEach(smContact -> System.out.println("🔍 Similar contact: " + smContact));
-        }
-    }
-
-    @Override
-    public void findByAddressPrefix(List<Contact> contacts, String addressPrefix) {
-        List<Contact> filteredList = contacts.stream().filter(c -> c.getAddress().startsWith(addressPrefix)).toList();
-        if (filteredList.isEmpty()) {
-            System.out.println("❗ No contacts found with the address prefix: " + addressPrefix);
-        } else {
-            filteredList.forEach(smContact -> System.out.println("🔍 Similar contact: " + smContact));
-        }
-    }
-
-    @Override
-    public void findByPhonePrefix(List<Contact> contacts, String phonePrefix) {
-        List<Contact> filteredList = contacts.stream().filter(c -> c.getPhone().startsWith(phonePrefix)).toList();
-        if (filteredList.isEmpty()) {
-            System.out.println("❗ No contacts found with the phone prefix: " + phonePrefix);
-        } else {
-            filteredList.forEach(smContact -> System.out.println("🔍 Similar contact: " + smContact));
-        }
-    }
 
     public void setScanner(Scanner scanner) {
         this.SC = scanner;
--- src/service/SearchAction.java
+++ src/service/SearchAction.java
@@ -14,13 +14,6 @@ public interface SearchAction {
 
     List<Contact> searchContactByPhone(List<Contact> contacts);
 
-    void findByNamePrefix(List<Contact> contacts, String namePrefix);
-
-    void findBySurnamePrefix(List<Contact> contacts, String surnamePrefix);
-
-    void findByAddressPrefix(List<Contact> contacts, String addressPrefix);
-
-    void findByPhonePrefix(List<Contact> contacts, String phonePrefix);
 
 
 }
