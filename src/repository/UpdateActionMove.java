3b565440d2236c4f626db98bbea4b7bd4be003d3
azidinkun <azidinkun@icloud.com>
2024-11-23 17:21:03 +0600

    added method for updateContactBySurname block try catch

--- src/repository/UpdateActionMove.java
+++ src/repository/UpdateActionMove.java
@@ -38,18 +38,23 @@ public class UpdateActionMove implements UpdateAction {
 
     @Override
     public void updateContactBySurname(List<Contact> contacts) {
-        List<Contact> foundContacts = search.searchContactBySurname(contacts);
-        if (foundContacts.isEmpty()) {
-            return;
-        } else {
-            System.out.print("Enter the index of the contact to update (1 to " + foundContacts.size() + "): ");
-            int userIndex = getValidIndex(foundContacts.size());
-            foundContacts.stream()
-                    .skip(userIndex)
-                    .findFirst()
-                    .ifPresent(contact -> updateContact(contacts, contacts.indexOf(contact)));
+        try {
+            for (Contact _ : contacts) {
+                List<Contact> foundContacts = search.searchContactBySurname(contacts);
+                if (foundContacts.isEmpty()) {
+                    return;
+                } else {
+                    System.out.print("Enter the index of the contact to update (1 to " + foundContacts.size() + "): ");
+                    int userIndex = getValidIndex(foundContacts.size());
+                    foundContacts.stream()
+                            .skip(userIndex)
+                            .findFirst()
+                            .ifPresent(contact -> updateContact(contacts, contacts.indexOf(contact)));
+                }
+            }
+        } catch (Exception e) {
+            e.fillInStackTrace();
         }
-
     }
 
     @Override
