caf844c3feaf8d828e41785704ab775fd344e67c
azidinkun <azidinkun@icloud.com>
2024-11-23 17:01:54 +0600

    added method for searchContactByName to check empty input and block try catch

--- src/repository/SearchActionMove.java
+++ src/repository/SearchActionMove.java
@@ -3,6 +3,8 @@ package repository;
 import model.Contact;
 import service.SearchAction;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,27 +14,36 @@ public class SearchActionMove implements SearchAction {
     Scanner SC = new Scanner(System.in);
 
     private final SearchActionByPrefixMove searchByPrefix;
+    private final CheckActionMove checkActionMove;
 
     public SearchActionMove() {
         searchByPrefix = new SearchActionByPrefixMove();
+        checkActionMove = new CheckActionMove();
     }
 
     @Override
     public List<Contact> searchContactByName(List<Contact> contacts) {
+        List<Contact> foundContacts = new ArrayList<>();
+        try {
+            System.out.print("Enter name to search: ");
+            String next = SC.nextLine();
+            checkActionMove.checkStringForEmpty(next);
+            foundContacts = contacts.stream()
+                    .filter(contact -> contact.getName().equals(next))
+                    .toList();
+            if (!foundContacts.isEmpty()) {
+                foundContacts.forEach(contact -> System.out.println("🔍 Contact Found: " + contact));
+            } else {
+                System.out.println("No contact found with the such name: " + next);
+                System.out.println("Trying to find similar contacts by name...");
+                searchByPrefix.findByNamePrefix(contacts, next);
+            }
 
-        System.out.print("Enter name to search: ");
-        String next = SC.next();
-        List<Contact> foundContacts = contacts.stream()
-                .filter(contact -> contact.getName().equals(next))
-                .toList();
-        if (!foundContacts.isEmpty()) {
-            foundContacts.forEach(contact -> System.out.println("🔍 Contact Found: " + contact));
-        } else {
-            System.out.println("No contact found with the such name: " + next);
-            System.out.println("Trying to find similar contacts by name...");
-            searchByPrefix.findByNamePrefix(contacts, next);
+        } catch (InputMismatchException e) {
+            e.fillInStackTrace();
+            System.err.print("❌ Invalid input. Please enter the name to search.\n");
+
         }
-
 
         return foundContacts;
     }
@@ -42,7 +53,8 @@ public class SearchActionMove implements SearchAction {
     public List<Contact> searchContactBySurname(List<Contact> contacts) {
 
         System.out.print("Enter surname to search: ");
-        String next = SC.next();
+        String next = SC.nextLine();
+        checkActionMove.checkStringForEmpty(next);
         List<Contact> foundContacts = contacts.stream()
                 .filter(contact -> contact.getSurname().equals(next))
                 .toList();
@@ -62,7 +74,8 @@ public class SearchActionMove implements SearchAction {
     public List<Contact> searchContactByAddress(List<Contact> contacts) {
 
         System.out.print("Enter address to search: ");
-        String next = SC.next();
+        String next = SC.nextLine();
+        checkActionMove.checkStringForEmpty(next);
         List<Contact> foundContacts = contacts.stream()
                 .filter(contact -> contact.getAddress().equals(next))
                 .toList();
@@ -83,7 +96,8 @@ public class SearchActionMove implements SearchAction {
     public List<Contact> searchContactByPhone(List<Contact> contacts) {
 
         System.out.print("Enter phone number to search: ");
-        String next = SC.next();
+        String next = SC.nextLine();
+        checkActionMove.checkStringForEmpty(next);
         List<Contact> foundContacts = contacts.stream()
                 .filter(contact -> contact.getPhone().equals(next))
                 .toList();
