1c41885970ccb26e245a29d53d6a3fdb99669909
azidinkun <azidinkun@icloud.com>
2024-11-28 12:16:09 +0600

    added output if contact will update

--- src/repository/UpdateActionMove.java
+++ src/repository/UpdateActionMove.java
@@ -180,6 +180,7 @@ public class UpdateActionMove implements UpdateAction {
             contactToUpdate.setSurname(newSurname);
             contactToUpdate.setAddress(newAddress);
             contactToUpdate.setPhone(validNumber);
+            System.out.println("✅ Contact added successfully!\n");
 
         } catch (InputMismatchException e) {
             System.err.println("\n❌ Error: " + e.getMessage() + " Contact not updated.\n");
