41cf4980635083bb16a1eb2a2b03978eb6ada8e7
azidinkun <azidinkun@icloud.com>
2024-11-30 23:53:54 +0600

    made the variables big

--- src/fileService/FileService.java
+++ src/fileService/FileService.java
@@ -12,15 +12,15 @@ import java.util.List;
 
 public class FileService {
 
-    private static final String tagContacts = "contacts";
+    private static final String TAG_CONTACTS = "contacts";
 
-    private static final String name = "name";
+    private static final String NAME = "name";
 
-    private static final String surname = "surname";
+    private static final String SURNAME = "surname";
 
     private static final String address = "address";
 
-    private static final String phone = "phone";
+    private static final String PHONE = "phone";
 
     public List<Contact> read() {
         List<Contact> contacts = new ArrayList<>();
@@ -28,13 +28,13 @@ public class FileService {
         try (FileReader fileReader = new FileReader("contacts.json")) {
             JsonObject jsonObject = JsonParser.parseReader(fileReader).getAsJsonObject();
 
-            JsonArray contactsArray = jsonObject.getAsJsonArray(tagContacts);
+            JsonArray contactsArray = jsonObject.getAsJsonArray(TAG_CONTACTS);
             for (JsonElement item : contactsArray) {
                 JsonObject contactsObject = item.getAsJsonObject();
-                String nameOfContact = contactsObject.get(name).getAsString();
-                String surnameOfContact = contactsObject.get(surname).getAsString();
+                String nameOfContact = contactsObject.get(NAME).getAsString();
+                String surnameOfContact = contactsObject.get(SURNAME).getAsString();
                 String addressOfContact = contactsObject.get(address).getAsString();
-                String phoneOfContact = contactsObject.get(phone).getAsString();
+                String phoneOfContact = contactsObject.get(PHONE).getAsString();
                 Contact contact = new Contact(nameOfContact, surnameOfContact, addressOfContact, phoneOfContact);
                 contacts.add(contact);
             }
@@ -50,15 +50,15 @@ public class FileService {
 
         for (Contact contact : contacts) {
             JsonObject contactObject = new JsonObject();
-            contactObject.add(name, new JsonPrimitive(contact.getName()));
-            contactObject.add(surname, new JsonPrimitive(contact.getSurname()));
+            contactObject.add(NAME, new JsonPrimitive(contact.getName()));
+            contactObject.add(SURNAME, new JsonPrimitive(contact.getSurname()));
             contactObject.add(address, new JsonPrimitive(contact.getAddress()));
-            contactObject.add(phone, new JsonPrimitive(contact.getPhone()));
+            contactObject.add(PHONE, new JsonPrimitive(contact.getPhone()));
             contactsArray.add(contactObject);
         }
 
         JsonObject jsonObject = new JsonObject();
-        jsonObject.add(tagContacts, contactsArray);
+        jsonObject.add(TAG_CONTACTS, contactsArray);
 
         try (FileWriter fileWriter = new FileWriter("contacts.json")) {
             Gson gson = new Gson();
