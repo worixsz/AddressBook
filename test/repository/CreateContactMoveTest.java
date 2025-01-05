f1aed5c715bd2eed6a498598b4be5e937b526b62
Azidin Amankulov <120017899+worixsz@users.noreply.github.com>
2025-01-05 22:23:11 +0600

    cleaned the code

--- test/repository/CreateContactMoveTest.java
+++ test/repository/CreateContactMoveTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
